38df6c774de1d2c1a6f612c4ff51b6b2a5d7e1d9
molnarbea <molnarbeatrix11@gmail.com>
2025-11-20 13:16:22 +0100

    tabla szerkezet

---
+++ tic_tac_toe.py
@@ -0,0 +1,26 @@
+def tabla_sor(minta):
+    print(minta*10)
+def tabla_oszlop(minta):
+    print(f"{minta}{minta:>{3}}{minta:>{3}}{minta:>{3}}")
+def kiir():
+    tabla_sor("*")
+    tabla_oszlop("*")
+    tabla_sor("*")
+    tabla_oszlop("*")
+    tabla_sor("*")
+    tabla_oszlop("*")
+    tabla_sor("*")
+
+
+
+
+
+
+
+# **********
+# *  *  *  *
+# **********
+# *  *  *  *
+# **********
+# *  *  *  *
+# **********
